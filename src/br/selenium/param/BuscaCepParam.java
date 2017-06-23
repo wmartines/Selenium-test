f833a27dc10356ec6aedde7d2f952e3ab6500ae2
rmartines <rma.wilson@gmail.com>
2017-06-23 02:32:48 -0300

    selenium-estudo

---
+++ src/br/selenium/param/BuscaCepParam.java
@@ -0,0 +1,15 @@
+package br.selenium.param;
+
+public class BuscaCepParam {
+
+	private String street;
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+}
--- src/br/selenium/test/BuscaCep.java
+++ src/br/selenium/test/BuscaCep.java
@@ -1,48 +1,83 @@
 package br.selenium.test;
 
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import io.github.bonigarcia.wdm.FirefoxDriverManager;
+import io.github.bonigarcia.wdm.ChromeDriverManager;
 
 public class BuscaCep {
 
 	WebDriver driver;
 
+	/**
+	 * Setup do driver feito com Maven
+	 * 
+	 */
 	@BeforeClass
 	public void setUp() {
-
-		FirefoxDriverManager.getInstance().setup();
-		driver = new FirefoxDriver();
-		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		
+		ChromeDriverManager.getInstance().setup();
+		driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get("http://www.buscacep.correios.com.br/sistemas/buscacep/");
 
 	}
 
-	@Test
-	public void firstTest() {
+	/**
+	 * Metodo responsavel por realizar pesquisa de endereco
+	 * 
+	 */
+	@Test(priority = 1)
+	public void invalidSearch() {
 
-		WebDriverWait wait = new WebDriverWait(driver, 10);
+		// Tentar realizar a acao dentro do bloco try
+		try {
 
-		driver.findElement(By.xpath(".//*[@id='Geral']/div/div/span[2]/label/input")).sendKeys("Milton Soares Neiva");
+			// Abre pagina para buscar cep
+			driver.get("http://www.buscacep.correios.com.br/sistemas/buscacep/");
 
-		driver.findElement(By.xpath(".//*[@id='Geral']/div/div/div[6]/input")).click();
+			// Declara espera de 10 segundos
+			WebDriverWait wait = new WebDriverWait(driver, 10);
 
-		wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(
-				By.xpath("html/body/div[1]/div[3]/div[2]/div/div/div[2]/div[2]/div[2]")));
+			// Envia parametro para o campo de busca
+			driver.findElement(By.xpath(".//*[@id='Geral']/div/div/span[2]/label/input")).sendKeys("Milton Soares Neiva");
 
-		Assert.assertTrue(driver.getPageSource().contains("05833-260"));
+			// Clica no botao pesquisar
+			driver.findElement(By.xpath(".//*[@id='Geral']/div/div/div[6]/input")).click();
 
+			// Aguarde no maximo 10 segundos ate o resultado da pesquisa ser exibido		
+			wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("html/body/div[1]/div[3]/div[2]/div/div/div[2]/div[2]/div[2]")));
+
+			// Certifique-se de que o resultado e true
+			Assert.assertTrue(driver.getPageSource().contains("05833-260"));
+
+			// Captura a excecao
+		} catch (Exception e) {
+
+			// Imprime excecao
+			System.out.println("Erro buscar por cep: " + e.getCause());
+		}
+
+	}
+	
+	@AfterMethod
+	public void afterMethod(ITestResult testResult){
+		
+		if(testResult.getStatus() == ITestResult.FAILURE){
+			
+			
+		}else{
+			
+			
+		}
 	}
 
 	@AfterClass
